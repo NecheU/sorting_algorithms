86b1fa88783fe7539be5fad2a9377963bea3ca81
NecheU <uzorgodsboy1@gmail.com>
2024-01-20 01:27:44 -0800

    sort.h

---
+++ README.md
@@ -0,0 +1,1 @@
+Team project between Uzor and Ene
---
+++ sort.h
@@ -0,0 +1,30 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s Doubly linked list node
+ * @n: int stored in the node
+ * @prev: Pointer to the prev element in the list
+ * @next: Pointer to the next elem im the list
+ */
+
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void swap(int *array, size_t i, size_t j);
+
+
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+#endif
