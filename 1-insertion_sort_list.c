97b5dd95d15da58a337bccc2a3a60155395d66ca
NecheU <uzorgodsboy1@gmail.com>
2024-01-23 06:35:04 -0800

    imsertion_sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts a list of int usimg doubly linked list
+ * @list: contents of the list to be sorted
+ *
+ * Return: nothing
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *comprd_node, *j;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+
+		return;
+
+	comprd_node = (*list)->next;
+
+	while (comprd_node)
+	{
+		 j = comprd_node->next;
+
+		 while (comprd_node->prev != NULL && comprd_node->n < comprd_node->prev->n)
+		 {
+			 comprd_node->prev->next = comprd_node->next;
+
+			 if (comprd_node != NULL)
+				 comprd_node->next->prev = comprd_node->prev;
+
+			 comprd_node->next = comprd_node->prev;
+			 comprd_node->prev = comprd_node->prev->prev;
+
+			 if (comprd_node->prev != NULL)
+				 comprd_node->prev->next = comprd_node;
+			 else
+				 *list = comprd_node;
+			 if (comprd_node->next != NULL)
+				 comprd_node->next->prev = comprd_node;
+
+			 print_list(*list);
+		 }
+		 comprd_node = j;
+	}
+}
---
+++ print_list.c
@@ -0,0 +1,22 @@
+#include "sort.h"
+
+/**
+ * print_list - Prints a list of int
+ *
+ * @list: the contents of the list to be printed
+ */
+void print_list(const listint_t *list)
+{
+	int o;
+
+	o = 0;
+	while (list)
+	{
+		if (o > 0)
+			printf(", ");
+		printf("%d", list->n);
+		o++;
+		list = list->next;
+	}
+	printf("\n");
+}
